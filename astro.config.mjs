3be60df65ef0d118bee1de479e32ad8b7b235771
EwenQuim <ewen.quimerch@gmail.com>
2023-04-24 00:18:41 +0200

    Styling the front page and footer/headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://reblog.quimerch.com",
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
