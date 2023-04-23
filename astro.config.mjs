5774f00e6d44cc41ea9f50a6fac74caa423e82c8
EwenQuim <ewen.quimerch@gmail.com>
2023-04-24 01:41:19 +0200

    ⚡️ Prefetch links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://reblog.quimerch.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    react(),
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [mdx(), sitemap(), react(), tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), prefetch()]
 });
