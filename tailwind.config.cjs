a7629dba482feb1c5d53e15b42f7345515c51329
EwenQuim <ewen.quimerch@gmail.com>
2025-06-05 16:19:02 +0200

    Silk background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Trebuchet MS"', '"Lucida Sans Unicode"', '"Lucida Grande"', '"Lucida Sans"', 'Arial', 'sans-serif'],
+        'mono': ['"Fira Code"', 'monospace'],
+      },
+      colors: {
+        'bg-primary': '#000000',
+        'text-primary': '#BBB',
+        'text-heading': '#222',
+        'text-link': '#888',
+        'text-link-hover': '#557799',
+        'text-article-link': 'blue',
+        'text-visited': 'purple',
+        'border-color': '#999',
+        'code-bg': '#f2f2f2',
+        'table-header-bg': '#eee',
+      },
+      maxWidth: {
+        'content': '40rem',
+      },
+    },
   },
   plugins: [],
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
 };
