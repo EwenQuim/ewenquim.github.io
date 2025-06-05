3eef6825f82b9498db1b2c169ffc3ed160d40173
EwenQuim <ewen.quimerch@gmail.com>
2025-06-05 18:22:03 +0200

    Updated astro to 5.9.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
-import prefetch from "@astrojs/prefetch";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://reblog.quimerch.com",
+	site: "https://ewenquim.github.io",
 	prefetch: true,
 	integrations: [
 		mdx(),
 		sitemap(),
 		react(),
-		prefetch(),
 		robotsTxt({ policy: [{ userAgent: "*", allow: "/" }] }),
 	],
 	vite: {
